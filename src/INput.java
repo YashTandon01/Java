22a6fd9bee842960d1c6b7d24b03390d62f554d7
YashTandon01 <yashtandon335@gmail.com>
2023-01-04 14:08:20 +0530

    Input in Java From User

---
+++ src/INput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class INput {
+    public static  void main(String[]args){
+
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number 1");
+            int a = sc.nextInt();
+            System.out.println("Enter the number 2");
+            int b = sc.nextInt();
+            int sum = a+b;
+        System.out.println("The Sum is\n"+ sum);
+
+
+
+}}
+
