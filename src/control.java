7609c91046192395003a68d726080f9e118ed4e5
YashTandon01 <yashtandon335@gmail.com>
2023-01-15 10:43:43 +0530

    Control Statements in Java

---
+++ src/control.java
@@ -0,0 +1,17 @@
+// print the largest of 2 numbers
+public class control {
+    public static  void main(String[]args)
+    {
+     int a = 12;
+     int b = 15;
+     if(a>b)
+     {
+         System.out.println("a is greater");
+     }
+     else
+     {
+         System.out.println("B is greater");
+     }
+    }
+
+}
